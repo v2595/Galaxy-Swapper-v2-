25cac61985d23f45a45c8b389c9a808f889f8087
Galaxy-Swapper-v2 <mteser6@gmail.com>
2024-07-01 12:38:43 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Galaxy_Swapper_v2.Workspace;
+using Galaxy_Swapper_v2.Workspace.ClientSettings;
+using Galaxy_Swapper_v2.Workspace.Properties;
+using Galaxy_Swapper_v2.Workspace.Utilities;
+using Serilog;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Galaxy_Swapper_v2
+{
+    public partial class App : Application
+    {
+        public static readonly string Config = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Galaxy-Swapper-v2-Config";
+        public static readonly string[] SubDirectories = { $"{Config}\\DLLS", $"{Config}\\Plugins", $"{Config}\\LOGS", $"{Config}\\Binaries" };
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Process[] currentprocess = Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName);
+            if (currentprocess.Length > 1)
+            {
+                Win32.SetProcessHigher(currentprocess);
+                Message.DisplaySTA("Info", "It has been detected that Galaxy Swapper is already running. The process will be brought to the front.\nIf there is an issue, please end the 'Galaxy Swapper v2' process in Task Manager and restart the application.");
+                Environment.Exit(0);
+            }
+
+            if (File.Exists($"{Config}\\Key.config")) //Remove old config folder
+            {
+                Directory.Delete(Config, true);
+            }
+
+            if (!Directory.Exists(Config)) //Create config folder
+            {
+                Directory.CreateDirectory(Config);
+            }
+
+            foreach (string sub in SubDirectories)
+            {
+                Directory.CreateDirectory(sub);
+            }
+
+            Output.Initialize(); //Serilog logger
+
+            Log.Information("Version {0}", Global.Version);
+            Log.Information("API-Version {0}", Global.ApiVersion);
+            Log.Information("Runtime {0}", RuntimeInformation.FrameworkDescription);
+            Log.Information("Config-Path: {0}", Config);
+
+            Settings.Initialize();
+            SwapLogs.Initialize();
+            UEFN.Initialize();
+            Binaries.Initialize();
+
+            base.OnStartup(e);
+        }
+        
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            Log.Fatal("Application caught a unexpected error and can not recover");
+            Log.Fatal(e.Exception.ToString());
+
+            Message.DisplaySTA("Error", $"Application caught a unexpected error and can not recover.\n{e.Exception}", discord: true);
+        }
+
+        private void App_Exit(object sender, ExitEventArgs e)
+        {
+            Log.Information("Shutting down application..");
+            Log.CloseAndFlush();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Galaxy-Swapper-v2-
-Fortnite Skin Swapper
+## Galaxy Swapper v2
+
+Revamp your Fortnite experience with our skin swapper designed for the latest version of Fortnite. This repository contains the source code for Galaxy Swapper v2.
+
+Note: It is recommended to download Galaxy Swapper v2 from [here](https://galaxyswapperv2.com/Downloads/InGame.php) instead of rebuilding it yourself.
+
+## What is Galaxy Swapper v2?
+
+Galaxy Swapper v2 is a free Fortnite skin changer built in WPF that allows you to modify your game files to replace a cosmetic you own with a cosmetic you desire, all with ease. We have also developed a simple and user-friendly user-interface for your convenience.
+
+## Social media links
+* [Discord](https://galaxyswapperv2.com/Discord)
+* [Website](https://galaxyswapperv2.com)
+
+## Packages used in our application
+
+* [CUE4Parse](https://github.com/FabianFG/CUE4Parse)
+* [DotNetZip](https://github.com/haf/DotNetZip.Semverd)
+* [K4os.Compression.LZ4](https://github.com/MiloszKrajewski/K4os.Compression.LZ4)
+* [Newtonsoft.Json](https://www.newtonsoft.com/json)
+* [Serilog](https://serilog.net)
+
+## Endpoints used in our application
+
+* [Fortnite-Api](https://dash.fortnite-api.com)
+
+## Authors
+
+* [@Wslt](https://github.com/CodeWslt)
+
+## License
+
+Galaxy Swapper v2 is licensed under [GNU General Public License v3.0](https://github.com/GalaxySwapperOfficial/Galaxy-Swapper-v2/blob/main/LICENSE)
+
+<a href="https://galaxyswapperv2.com/Guilded"><img src="https://cdn.discordapp.com/attachments/1122580592370921494/1129255436281983046/ServerBanner2.jpg"></a>
